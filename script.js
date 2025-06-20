116750522a89bcca80d27b2f7ee4d49b3e48f68b
Шатун Олексій Андрійович <124837700+Nayntar@users.noreply.github.com>
2025-06-20 03:21:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = document.getElementById('skillsChart').getContext('2d');
+  new Chart(ctx, {
+    type: 'radar',
+    data: {
+      labels: [
+        'HTML/CSS', 'C++', 'Бази даних', 'Arduino', 'Blender',
+        'Відповідальність', 'Командна робота', 'Швидке навчання', 'Англійська'
+      ],
+      datasets: [{
+        label: 'Рівень володіння',
+        data: [75, 60, 70, 55, 35, 80, 90, 90, 78],
+        backgroundColor: 'rgba(0, 122, 204, 0.2)',
+        borderColor: '#007acc',
+        pointBackgroundColor: '#007acc',
+        borderWidth: 2
+      }]
+    },
+    options: {
+      responsive: false,
+      scales: {
+        r: {
+          suggestedMin: 0,
+          suggestedMax: 100
+        }
+      },
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    }
+  });
+});
+
+
+// AOS
+AOS.init();
+
+// Тема
+document.getElementById("themeToggle").addEventListener("click", () => {
+  const current = document.documentElement.className;
+  const next = current === "light" ? "dark" : "light";
+  document.documentElement.className = next;
+  localStorage.setItem("theme", next);
+});
